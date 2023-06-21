911b00a5dc1581b1fa82cc5cb9100c23111b6b4a
matejmatik <matejmatik@outlook.com>
2023-06-21 05:32:56 +0200

    Nadaljevanje aplikacije.

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+PODATKOVNA_BAZA = "vremenski_podatki.db"
 
 
 def create_app():
     app = Flask(__name__)   # za postavitev "Flaska"
     app.config['SECRET_KEY'] = 'skrivnost'   # za enkripcijo seje in piškotkov
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{PODATKOVNA_BAZA}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -11,4 +18,12 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    import .models as models
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path('website/' + PODATKOVNA_BAZA):
+        db.create_all(app=app)
+        print('Podatkovna baza je ustvarjena.')
--- website/models.py
+++ website/models.py
@@ -1,1 +1,22 @@
- 
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+# func.now() -- Pridobi trenutni cas.
+
+
+class Uporabnik(db.Model, UserMixin):
+    _id = db.Column(db.Integer, primary_key=True)
+    elektronski_naslov = db.Column(db.String(150), unique=True)
+    ime = db.Column(db.String(40))
+    priimek = db.Column(db.String(50))
+    geslo = db.Column(db.String(150))
+    izbrane_postaje = db.relationship('IzbranaPostaja') # Uporabimo ime razreda (Z veliko)
+
+
+class VremenskaPostaja(db.Model):
+    pass
+
+class IzbranaPostaja(db.Model):
+
+    # Tuji kljuc
+    id_uporabnik = db.Column(db.Integer, db.ForeignKey('uporabnik._id')) # Uporabimo ime razreda (z malo)
